827357ede933205fcde9e2d5a2309131bb44b967
bartolemio <bartolemio@gmail.com>
2020-09-21 09:56:21 +0300

    Updates

---
+++ precache-manifest.8d06e8c10be4826634c48417ba493ec1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8aa345ea104abcc02a2622d5eb39c963",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "d72e1a822779a9bf946a",
+    "url": "/todo-react/static/css/main.d6d3dde0.chunk.css"
+  },
+  {
+    "revision": "0d8910c929998331837f",
+    "url": "/todo-react/static/js/2.7d5ceba7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.7d5ceba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d72e1a822779a9bf946a",
+    "url": "/todo-react/static/js/main.6cd48ee3.chunk.js"
+  },
+  {
+    "revision": "ceaaf66d93b7f2e5a06c",
+    "url": "/todo-react/static/js/runtime-main.467c2cfd.js"
+  }
+]);
